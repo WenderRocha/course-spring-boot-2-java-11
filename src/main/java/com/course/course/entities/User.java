47309c5fa3cfa13091bba091e57c66571c38b22a
Wender Rocha <Wender_Dev@hotmail.com>
2021-10-22 20:56:54 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/course/course/entities/User.java
+++ src/main/java/com/course/course/entities/User.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -44,6 +46,7 @@ public class User implements Serializable {
 	@NonNull
 	private String password;
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	@Setter(AccessLevel.NONE)
 	private List<Order> orders = new ArrayList<>();
